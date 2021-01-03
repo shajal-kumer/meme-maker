65e5bb16cf72213edc2a460a50b25117f87a77d1
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-01-03 10:11:08 +0600

    image handle done

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // select all class or id
-const topLine = document.querySelector("#topline");
-const bottomLine = document.querySelector("#bottomline");
-const download = document.querySelector("#download");
-const selectImage = document.querySelector("#selectimage");
-const canvas = document.querySelector("#c");
+const topLine = document.querySelector(".topline");
+const bottomLine = document.querySelector(".bottomline");
+const download = document.querySelector(".download");
+const selectImage = document.querySelector(".selectimage");
+const canvas = document.querySelector(".c");
 const context = canvas.getContext("2d");
+let image = null;
+let topLineText = "";
+let bottomLineText = "";
 
 // 1st process the selected image
 selectImage.addEventListener("change", (e) => {
 	const reader = new FileReader();
 	const file = e.target.files[0];
-    reader.readAsDataURL(file);
-    reade
+	reader.readAsDataURL(file);
+	reader.onload = function (e) {
+		const data = e.target.result;
+
+		image = new Image();
+		image.src = data;
+		image.onload = function () {
+			drawMeme(this, null, null);
+		};
+	};
 });
