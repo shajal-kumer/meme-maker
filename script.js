8640735905d5991452eefe755a7003361527755b
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-01-04 09:21:05 +0600

    font size, family and color added

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ const bottomLine = document.querySelector(".bottomline");
 const download = document.querySelector(".download");
 const selectImage = document.querySelector(".selectimage");
 const canvas = document.querySelector(".c");
+const fillColor = document.querySelector(".fill-color");
+const strokeColor = document.querySelector(".stroke-color");
+const fontFamily = document.querySelector(".font-family");
+const fontSize = document.querySelector(".font-size");
+
 const context = canvas.getContext("2d");
 let image = null;
 let topLineText = "";
 let bottomLineText = "";
+let fillColorValue = "#ffffff";
+let strokeColorValue = "#000000";
+let fontFamilyValue = "Impact";
+let fontSizeValue = "36";
 
 // 1st process the selected image
 selectImage.addEventListener("change", (e) => {
@@ -38,9 +47,9 @@ bottomLine.addEventListener("input", (e) => {
 function drawMeme(image, topLine, bottomLine) {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
-	context.font = "32px Impact";
-	context.fillStyle = "white";
-	context.strokeStyle = "black";
+	context.font = `${fontSizeValue}px ${fontFamilyValue}`;
+	context.fillStyle = fillColorValue;
+	context.strokeStyle = strokeColorValue;
 	context.lineWidth = 3;
 	context.textAlign = "center";
 
@@ -68,3 +77,20 @@ function downloadMem() {
 	topLine.value = "";
 	bottomLine.value = "";
 }
+
+fillColor.addEventListener("input", (e) => {
+	fillColorValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+strokeColor.addEventListener("input", (e) => {
+	strokeColorValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+fontFamily.addEventListener("change", (e) => {
+	fontFamilyValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+fontSize.addEventListener("change", (e) => {
+	fontSizeValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
