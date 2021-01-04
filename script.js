cc863ce6f0deb5a6cc114a84d849b90a370253ac
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-01-04 09:36:03 +0600

    some code formate and final edition

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// select all class or id
+// selectors
 const topLine = document.querySelector(".topline");
 const bottomLine = document.querySelector(".bottomline");
 const download = document.querySelector(".download");
@@ -9,6 +9,7 @@ const strokeColor = document.querySelector(".stroke-color");
 const fontFamily = document.querySelector(".font-family");
 const fontSize = document.querySelector(".font-size");
 
+// Gloval variables
 const context = canvas.getContext("2d");
 let image = null;
 let topLineText = "";
@@ -18,33 +19,77 @@ let strokeColorValue = "#000000";
 let fontFamilyValue = "Impact";
 let fontSizeValue = "36";
 
-// 1st process the selected image
+// Event Listenners
+
+// 1st process the image and create new image
 selectImage.addEventListener("change", (e) => {
+	// create a new object for file reader
 	const reader = new FileReader();
+
+	// get the file link
 	const file = e.target.files[0];
+
+	// read the file
 	reader.readAsDataURL(file);
+
+	// after load the file --> create new image
 	reader.onload = function (e) {
 		const data = e.target.result;
 
+		// create a new object for image
 		image = new Image();
+		// set the src of image
 		image.src = data;
+
+		// after load the image draw the image on canvas
 		image.onload = function () {
+			// after load image --> draw the image in canvas
 			drawMeme(this, null, null);
 		};
 	};
 });
 
+// handle the topline input text
 topLine.addEventListener("input", (e) => {
 	topLineText = e.target.value;
 	drawMeme(image, topLineText, bottomLineText);
 });
 
+// handle the bottomine input text
 bottomLine.addEventListener("input", (e) => {
 	bottomLineText = e.target.value;
 	drawMeme(image, topLineText, bottomLineText);
 });
 
+// handle the fill color input
+fillColor.addEventListener("input", (e) => {
+	fillColorValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+// handle the stroke color input
+strokeColor.addEventListener("input", (e) => {
+	strokeColorValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+// handle the font family input
+fontFamily.addEventListener("change", (e) => {
+	fontFamilyValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+// handle the font size input
+fontSize.addEventListener("change", (e) => {
+	fontSizeValue = e.target.value;
+	drawMeme(image, topLineText, bottomLineText);
+});
+
+// download handler listerner
+download.addEventListener("click", downloadMem);
+
+// functions
+
+// draw meme function
 function drawMeme(image, topLine, bottomLine) {
+	// clear the canvas before drawing
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
 	context.font = `${fontSizeValue}px ${fontFamilyValue}`;
@@ -67,9 +112,8 @@ function drawMeme(image, topLine, bottomLine) {
 	}
 }
 
-download.addEventListener("click", downloadMem);
-
 function downloadMem() {
+	// get the canvas as image data url
 	const img = canvas.toDataURL("image/png");
 	download.href = img;
 	download.download = "my-image.png";
@@ -77,20 +121,3 @@ function downloadMem() {
 	topLine.value = "";
 	bottomLine.value = "";
 }
-
-fillColor.addEventListener("input", (e) => {
-	fillColorValue = e.target.value;
-	drawMeme(image, topLineText, bottomLineText);
-});
-strokeColor.addEventListener("input", (e) => {
-	strokeColorValue = e.target.value;
-	drawMeme(image, topLineText, bottomLineText);
-});
-fontFamily.addEventListener("change", (e) => {
-	fontFamilyValue = e.target.value;
-	drawMeme(image, topLineText, bottomLineText);
-});
-fontSize.addEventListener("change", (e) => {
-	fontSizeValue = e.target.value;
-	drawMeme(image, topLineText, bottomLineText);
-});
